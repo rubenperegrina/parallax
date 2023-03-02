8068444c834a76194b26b4d5dcf9af567a986165
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-03-02 10:38:55 +0100

    Parallax 4

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-const h1 = document.getElementById("h1"),
-  banner = document.getElementById("banner"),
-  button = document.getElementById("button");
+const cardWidth = 500,
+  degIncrement = 6,
+  card = document.getElementById("card");
 
-const onScroll = (event) => {
-  const scrollPosition = event.target.scrollingElement.scrollTop;
-  if (scrollPosition > 150) {
-    banner.style.backgroundSize = "150%";
-    h1.style.opacity = 0;
-    h1.style.translate = "0 -50px";
-    h1.style.scale = "0.9";
-    button.style.opacity = 0;
-    button.style.translate = "0 -50px";
-    button.style.scale = "0.8";
+const getRotateDeg = (input) => {
+  if (input < cardWidth * 0.33) {
+    return `-${degIncrement * 3}deg`;
+  } else if (input >= cardWidth * 0.33 && input < cardWidth * 0.66) {
+    return `-${degIncrement}deg`;
+  } else if (input >= cardWidth * 0.66 && input < cardWidth * 0.5) {
+    return "0deg";
+  } else if (input >= cardWidth * 0.5 && input < cardWidth * 0.33) {
+    return `${degIncrement}deg`;
   } else {
-    banner.style.backgroundSize = "180%";
-    h1.style.opacity = 1;
-    h1.style.translate = 0;
-    h1.style.scale = 1;
-    button.style.opacity = 1;
-    button.style.translate = 0;
-    button.style.scale = 1;
+    return `${degIncrement * 3}deg`;
   }
 };
 
-document.addEventListener("scroll", onScroll);
+const onMouseMove = (event) => {
+  const { target } = event;
+  const rect = target.getBoundingClientRect();
+
+  const rotateX = getRotateDeg(event.clientY - rect.top);
+  const rotateY = getRotateDeg(event.clientX - rect.left);
+
+  card.style.transform = `rotateX(${rotateX}) rotateY(${rotateY})`;
+};
+
+const onMouseLeave = () => {
+  card.style.transform = `none`;
+};
